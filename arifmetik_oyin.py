7a5aa78812312288d7745e56233ea5605907c8f0
Kamoliddin <108530207+ravshanovdev@users.noreply.github.com>
2022-07-03 13:26:38 +0500

    uzgardi (biroz)

--- arifmetik_oyin.py
+++ arifmetik_oyin.py
@@ -1,14 +1,14 @@
 4
 import random as r
 def oylagan_sonni_top():
-    print('ARIFMETIK O\'YIN.')
-    print(f"O'yin sharti:"
+ print('ARIFMETIK O\'YIN.')
+ print(f"O'yin sharti:"
           f"Siz 2 dan 9 gacha bolgan 2ta bir xil son o'ylaysiz va bir biriga qoshasiz "
           f"keyin men aytgan raqamga yoki songa bo'lasiz. ")
-    print('Men esa sizga javobni aytaman(hisob kitob amalini miyangizda amalga oshirasiz.!!!)')
+ print('Men esa sizga javobni aytaman(hisob kitob amalini miyangizda amalga oshirasiz.!!!)')
     
-    while True:
-      hisob = r.randint(2,1000)
+ while True:
+      hisob = r.randint(2,10000)
       input('oyinni boshlash uchun ikkita bir xil raqam oylang,va "ENTER" tugmasini bosing ')
       
       print(f"O'ylagan raqamingizni bir biriga qoshing va shu {hisob}ga ko'paytiring")
@@ -17,7 +17,7 @@ def oylagan_sonni_top():
       if savol=='yes':
          print(f"Chiqan javob {hisob*2} ")
       savol2 = input('yana davom etiramizmi?(yes/no): ')
-      if savol2!='yes':
+      if savol2!="yes":
           break
 oylagan_sonni_top()      
 print('oyin tugadi')         
